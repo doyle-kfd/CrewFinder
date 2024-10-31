d74144facdee8b1deaca91052886ec50926cb938
Kevin Doyle <doyle.kfd@gmail.com>
2024-10-31 08:57:48 +0000

    Config: Added placeholder views for accounts and trips apps

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def signup(request):
+    return HttpResponse("Signup page")
+
+def login(request):
+    return HttpResponse("Login page")
--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def create_trip(request):
+    return HttpResponse("Create Trip page")
+
+def view_trips(request):
+    return HttpResponse("View Trips page")

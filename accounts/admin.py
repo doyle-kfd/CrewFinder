0460f7948a614eb27e97b68acde378f51a029c3e
Kevin Doyle <doyle.kfd@gmail.com>
2024-11-05 12:15:34 +0000

    Feat/Style: Added functionality to admin dashboard to modify user account. Styled user listing on admin page for readability

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,24 +10,30 @@ class CustomUserAdmin(UserAdmin):
     ordering = ('date_joined',)
 
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('role', 'bio', 'experience_level')}),
+        (None, {'fields': ('role', 'bio', 'experience_level', 'profile_completed')}),
     )
 
     list_editable = ('is_active',)
 
     def get_queryset(self, request):
+        # Allow administrators to view all users, including inactive ones
         qs = super().get_queryset(request)
-        # Limit Administrators to only seeing Captain and Crew users
-        if request.user.role == User.ADMINISTRATOR and not request.user.is_superuser:
-            return qs.filter(role__in=[User.CAPTAIN, User.CREW])
-        return qs
+        if request.user.is_superuser or request.user.role == User.ADMINISTRATOR:
+            return qs
+        return qs.filter(is_active=True)  # Default for non-admin users
 
     def has_change_permission(self, request, obj=None):
-        # Allow Administrators to edit only Captain and Crew users
-        if request.user.is_superuser:
+        # Allow administrators to edit user profiles
+        if request.user.is_superuser or request.user.role == User.ADMINISTRATOR:
             return True
-        if request.user.role == User.ADMINISTRATOR and obj and obj.role in [User.CAPTAIN, User.CREW]:
-            return True
-        return False
+        return super().has_change_permission(request, obj)
+
+    def has_add_permission(self, request):
+        # Allow administrators to add new users
+        return request.user.is_superuser or request.user.role == User.ADMINISTRATOR
+
+    def has_delete_permission(self, request, obj=None):
+        # Allow administrators to delete users
+        return request.user.is_superuser or request.user.role == User.ADMINISTRATOR
 
 admin.site.register(User, CustomUserAdmin)

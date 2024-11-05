5e68a9ef4477a9e2bf3a23b409949e6c2bc82e60
Kevin Doyle <doyle.kfd@gmail.com>
2024-11-05 14:51:29 +0000

    Feat: modified administrator privelages to enable only suitable access to fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,3 @@
-# accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
@@ -9,31 +8,48 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ('username', 'email')
     ordering = ('date_joined',)
 
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('role', 'bio', 'experience_level', 'profile_completed')}),
+    # Display relevant fields, excluding permissions
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'role', 'bio', 'experience_level', 'is_active', 'profile_completed')
+        }),
+        ('Permissions', {
+            'fields': ('is_staff',),
+            'classes': ('collapse',),
+        }),
     )
 
-    list_editable = ('is_active',)
+    # Restrict certain fields based on the user's role
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        if not request.user.is_superuser and request.user.role == User.ADMINISTRATOR:
+            # Remove 'is_staff' from view for Administrators
+            fieldsets = (
+                (None, {'fields': ('username', 'email', 'role', 'bio', 'experience_level', 'is_active', 'profile_completed')}),
+            )
+        return fieldsets
 
     def get_queryset(self, request):
-        # Allow administrators to view all users, including inactive ones
         qs = super().get_queryset(request)
-        if request.user.is_superuser or request.user.role == User.ADMINISTRATOR:
-            return qs
-        return qs.filter(is_active=True)  # Default for non-admin users
-
-    def has_change_permission(self, request, obj=None):
-        # Allow administrators to edit user profiles
-        if request.user.is_superuser or request.user.role == User.ADMINISTRATOR:
-            return True
-        return super().has_change_permission(request, obj)
+        # Exclude superusers from Administrator's view
+        if not request.user.is_superuser and request.user.role == User.ADMINISTRATOR:
+            return qs.filter(is_superuser=False)
+        return qs
 
     def has_add_permission(self, request):
-        # Allow administrators to add new users
-        return request.user.is_superuser or request.user.role == User.ADMINISTRATOR
+        # Prevent Administrator from adding new users
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        # Prevent Administrator from modifying superuser accounts or permissions
+        if obj and obj.is_superuser and request.user.role == User.ADMINISTRATOR:
+            return False
+        return super().has_change_permission(request, obj)
 
     def has_delete_permission(self, request, obj=None):
-        # Allow administrators to delete users
-        return request.user.is_superuser or request.user.role == User.ADMINISTRATOR
+        # Prevent Administrator from deleting superuser accounts
+        if obj and obj.is_superuser and request.user.role == User.ADMINISTRATOR:
+            return False
+        return super().has_delete_permission(request, obj)
 
 admin.site.register(User, CustomUserAdmin)
